127e5ae64cc3acf3898444dd0ea30c0225d7aaeb
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2026-05-31 05:22:04 -0300

    Agregar base para conexion a la db

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import pug from 'pug';
+import { Sequelize, DataTypes } from 'sequelize';
 
 
 // Server options
 const app = express();
 const PORT = process.env.PORT;
+
+// Database connection
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	dialect: process.env.DB_DIALECT
+})
 
 // Set up view engine.
 app.set('view engine', 'pug');
@@ -23,6 +31,13 @@ app.get('/login', (req, res) => {
 });
 
 // Run server
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 	console.log(`Fotaza listening on port ${PORT}`);
+	// Test database connection.
+	try {
+		await sequelize.authenticate();
+		console.log('Database connection has been established successfully.');
+	} catch (error) {
+		console.error('Unable to connect to the database:', error);
+	}
 });
