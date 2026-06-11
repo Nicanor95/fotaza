19bfbc2d1edb90d66a8d68a240e13f4b414fd2c4
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2026-06-11 03:24:47 -0300

    Change architecture to make it easier to understand

--- controller/wall.js
+++ controller/wall.js
@@ -5,44 +5,49 @@ import { Op, fn, col, where } from "sequelize";
 
 export async function buildWall(terms) {
 	const tags = terms.tags
-	let images;
 
-	if (!Array.isArray(tags) || tags.length === 0) { // If not array or array empty
-		images = await Imagen.findAll({
-			include: [
-				{
-					model: Publicacion,
-					attributes: ['titulo']
-				}
-			]
-		});
-	} else {
-		images = await Imagen.findAll({
-			include: [ //Include the tags to the images found
-				{
-					model: Tag,
-					attributes: [],
-					through: { attributes: [] },
-					where: {
-						nombre: {
-							[Op.in]: tags // Keep the ones in the array
-						}
+	// Build object for the get all posts request.
+	const getAll = {
+		include: [
+			{
+				model: Publicacion,
+				attributes: ['titulo']
+			}
+		]
+	};
+
+	// Build object for the get all posts according to tags request.
+	const getAccordingToTags = {
+		include: [ //Include the tags to the images found
+			{
+				model: Tag,
+				attributes: [],
+				through: { attributes: [] },
+				where: {
+					nombre: {
+						[Op.in]: tags // Keep the ones in the array
 					}
-				},
-				{
-					model: Publicacion,
-					attributes: ['titulo']
 				}
-			],
-			group: ['Imagen.id', 'Publicacion.id'],
-			having: where(fn('COUNT', col('Tags.id')), tags.length) 
-			/* 
-			 * Group by imagen.id and count the amount of tag columns
-			 * if the count is the same as the array length,
-			 * then it has all of the tags. Keep those.
-			 */
-		})
+			},
+			{
+				model: Publicacion,
+				attributes: ['titulo']
+			}
+		],
+		group: ['Imagen.id', 'Publicacion.id'],
+		having: where(fn('COUNT', col('Tags.id')), tags.length) 
+		/* 
+		 * Group by imagen.id and count the amount of tag columns
+		 * if the count is the same as the array length,
+		 * then it has all of the tags. Keep those.
+		 */
 	}
+
+	// Select request accordingly
+	// Non-array and empty arrays use getAll, and obtain all posts. Otherwise they consider tags with getAccordingToTags.
+	const request = (!Array.isArray(tags) || tags.length === 0) ? getAll : getAccordingToTags;
+
+	const images = await Imagen.findAll(request);
 
 	let shapedResults = images.map((image) => ({
 		id: image.id,
